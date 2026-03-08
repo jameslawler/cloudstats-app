f1fef714b2fc910566865dbbd7f7c0de6f8f432c
jameslawler <github@jameslawler.com>
2026-03-08 20:18:09 +0000

    Update statistics api getSiteId logic

--- src/api/statistics.ts
+++ src/api/statistics.ts
@@ -4,10 +4,22 @@ import { getStatistics } from '../db/repositories/statistics';
 
 const app = new Hono<{ Bindings: Env }>();
 
+const getSiteId = (requestUrl: string) => {
+	const redundantSubDomains = ['s.', 'www.'];
+	const url = new URL(requestUrl);
+
+	for (const subDomain of redundantSubDomains) {
+		if (url.hostname.startsWith(subDomain)) {
+			return url.hostname.slice(subDomain.length);
+		}
+	}
+
+	return url.hostname;
+};
+
 app.get('/', async (c) => {
 	const db = getDb(c.env.DB);
-	const url = new URL(c.req.url);
-	const siteId = url.hostname;
+	const siteId = getSiteId(c.req.url);
 
 	const stats = await getStatistics(db, siteId);
 
